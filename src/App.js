c375a42b439c9ee42238d1f05a8aaac42529c572
Piotr Szpinda <you@example.com>
2025-04-07 02:07:17 +0200

    fix dropdownMenu horizontal scrolling

--- src/App.js
+++ src/App.js
@@ -426,6 +426,7 @@ const DropdownMenu = styled.div`
   box-shadow: 0 -5px 15px rgba(0, 0, 0, 0.3);
   overflow-y: auto;
   will-change: transform;
+  overflow-x: hidden;
 `;
 
 // const DropdownMenu = styled.div`
